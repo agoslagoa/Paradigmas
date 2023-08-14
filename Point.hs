5affbbd663f5b6a58c0323f1301c94b0bd54226b
Tomás <tommycass55@gmail.com>
2023-08-14 19:15:58 -0300

    Módulos

--- Point.hs
+++ Point.hs
@@ -3,7 +3,8 @@ module Point ( Point, newP, difP)
 
 import City 
 
-data Point = Poi Int Int deriving (Eq, Show)
+--Este constructor toma dos argumentos Int, que representan las coordenadas x e y del punto en el plano cartesiano.
+data Point = Poi Int Int deriving (Eq, Show) --Las derivaciones Eq y Show permiten realizar comparaciones de igualdad y mostrar puntos en forma de cadena.
 
 newP :: Int -> Int -> Point
 difP :: Point -> Point -> Float  -- distancia absoluta
---
+++ main.hs
@@ -0,0 +1,5 @@
+import Point
+
+main :: IO ()
+main = do
+    -- Código que realiza operaciones de entrada y salida
