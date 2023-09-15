07474aaf6614fb92d8650554916a1f5ba53f0a89
Tomás Castro <127274539+tommycass@users.noreply.github.com>
2023-09-15 15:35:34 -0300

    Update EmptyQueueState.java

--- TP2/EmptyQueueState.java
+++ TP2/EmptyQueueState.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class EmptyQueueState extends QueueState {
 	
-	public static final String queueIsEmpty = "Queue is empty";
+   public static final String queueIsEmpty = "Queue is empty";
 	
     public Object head(ArrayList<Object> objects) { throw new Error(queueIsEmpty); }
 
