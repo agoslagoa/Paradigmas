b004af5c60bf39c67a1229c6a6011315e002166a
agoslagoa <127276970+agoslagoa@users.noreply.github.com>
2023-09-15 17:11:32 -0300

    Actualizar NonEmptyQueueState.java

--- TP2/NonEmptyQueueState.java
+++ TP2/NonEmptyQueueState.java
@@ -4,13 +4,12 @@ import java.util.ArrayList;
 
 public class NonEmptyQueueState extends QueueState {
 
-    public Object head(ArrayList<Object> objects) { return objects.get(0); }
-
     public Object take(ArrayList<Object> objects) {
         Object firstElement = objects.get(0);
         objects.remove(0);
         return firstElement;
-    }
+    
+    public Object head(ArrayList<Object> objects) { return objects.get(0); }
 
     public boolean isEmpty() { return false; }
 }
