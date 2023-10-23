381a63bf4d3fadf71972a5cf4d6d101a302cfd18
Tomás Castro <127274539+tommycass@users.noreply.github.com>
2023-10-22 22:26:52 -0300

    Update Orientation.java

--- TP3/Orientation.java
+++ TP3/Orientation.java
@@ -4,10 +4,8 @@ public abstract class Orientation {
 
     public abstract String getCardinalPoint();
 
-    // Obtener el punto cardinal a la derecha
     public abstract Orientation getRight();
 
-    // Obtener el punto cardinal a la izquierda
     public abstract Orientation getLeft();
 
 	public abstract Position computeNewCoordinates( Position currentPosition ); 
