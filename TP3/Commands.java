c209b666101078fe4644ef09af8875e65fc7f27c
Tomás Castro <127274539+tommycass@users.noreply.github.com>
2023-10-22 22:25:01 -0300

    Update Commands.java

--- TP3/Commands.java
+++ TP3/Commands.java
@@ -3,9 +3,6 @@ package nemo;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-//import java.util.ArrayList;
-//import java.util.Arrays;
-
 public abstract class Commands {
 	
 	private static ArrayList<Commands> commands = new ArrayList<>(Arrays.asList(new AscendCommand(), new DescendCommand(), new ReleaseCapsuleCommand(), new MoveForwardCommand(), new TurnRightCommand(), new TurnLeftCommand()));
